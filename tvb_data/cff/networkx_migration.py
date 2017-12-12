95b5603fdd9db1a14332c8de69314a979b2116cd
dan.pop <popdanvlad87@gmail.com>
2017-12-12 14:44:42 +0200

    TVB-2322 added TVB copyright header, moved description into pydoc

--- tvb_data/cff/networkx_migration.py
+++ tvb_data/cff/networkx_migration.py
@@ -1,3 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+#
+# TheVirtualBrain-Framework Package. This package holds all Data Management, and
+# Web-UI helpful to run brain-simulations. To use it, you also need do download
+# TheVirtualBrain-Scientific Package (for simulators). See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2017, Baycrest Centre for Geriatric Care ("Baycrest") and others
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
+
+"""
+.. moduleauthor:: Dan Pop <dan.pop@codemart.ro>
+"""
+
 import networkx
 import tempfile
 import os
@@ -10,9 +44,9 @@ new_cff_path = 'connectivities.cff'
 temp_folder = "cff_data\\"
 
 
-# from a given Graph object store nodes and edges in separate files preparing them for loading in 2.0
 def _store_intermediate_nodes_and_edges(net, data_file, name):
     """
+    from a given Graph object store nodes and edges in separate files preparing them for loading in 2.0
     :param net: Graph stored in version 1.1
     :param data_file: temp_folder: path to folder where intermediate results will be stored
     :param name: name of the network
@@ -22,9 +56,9 @@ def _store_intermediate_nodes_and_edges(net, data_file, name):
     networkx.write_gpickle(net.nodes(data=True), data_file + "_nodes" + ".gpickle")
 
 
-# Reads the nodes and edges files stored in the 1.1 version and build a new Graph compatible with 2.0
 def _build_and_store_new_graph(data_file, name=""):
     """
+    Reads the nodes and edges files stored in the 1.1 version and build a new Graph compatible with 2.0
     :param data_file: path to temporary directory
     :param name: name of the network
     :return: new Graph compatible with version 2.0
@@ -38,10 +72,9 @@ def _build_and_store_new_graph(data_file, name=""):
     return net
 
 
-# This method will be called from an evironment with networkX 1.1 version installed
-# Given a cff file path load all networks and store intermediate nodes and edges for each of them
 def store_with_version_1(old_cff_path, temp_folder):
     """
+    Given a cff file path load all networks and store intermediate nodes and edges for each of them
     :param old_cff_path: path to cff file from version 1.1
     :param temp_folder: path to folder where intermediate results will be stored
     :return: store all intermediate nodes and graphs in separate files
@@ -56,10 +89,9 @@ def store_with_version_1(old_cff_path, temp_folder):
             _store_intermediate_nodes_and_edges(net.data, temp_folder, net.name)
 
 
-# This method will be called from an evironment with networkX 2.0 version installed
-# Given a cff file path load the cff object, store the new network data and save to a new cff file
 def create_new_version(old_cff_path, temp_folder, new_cff_path):
     """
+    Given a cff file path load the cff object, store the new network data and save to a new cff file
     :param old_cff_path: path to cff file from version 1.1
     :param temp_folder: path to folder where intermediate results are stored
     :param new_cff_path: path where new cff file is stored
